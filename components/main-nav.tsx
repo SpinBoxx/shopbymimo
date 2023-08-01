a84b4452236c7e8ba9461c05606a60a194a73dc4
SpinBoxx <quentin.mimault@orange.fr>
2023-08-01 22:04:23 +0200

    [FEAT] Added orders page

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -40,6 +40,11 @@ const MainNav = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
       active: pathname === `/${params.storeId}/products`,
     },
     {
+      href: `/${params.storeId}/orders`,
+      label: "Orders",
+      active: pathname === `/${params.storeId}/orders`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
